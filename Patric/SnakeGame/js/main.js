db8fa154eb3aa59b2fb33bcd2e4ef506101985a8
PatriksM <pmorevs@gmail.com>
2026-02-06 14:40:40 +0200

    food random position in x direction

--- Patric/SnakeGame/js/main.js
+++ Patric/SnakeGame/js/main.js
@@ -5,9 +5,11 @@ const BOX = 32;
 // total size of playground is 608 x 608, there are 19 squares in each direction, that means that each small square is of size 32x32
 
 let foodCoords = {
-    x: 1,
+    x: Math.floor(1 + 17 * Math.random()),
     y: 3
 }
+//Math.random funciton returns a randome value from 0 to 1 not including 1 or 0 
+//Math.floor function cuts the decimal part of the number, t.i. if you have Math.floor(5.6459) = 5 
 
 let myPlayground = new Image(); //creates and image object
 myPlayground.src = "img/ground.png"; //shows the path to the image
@@ -17,7 +19,7 @@ food.src = "img/mango.png";
 
 function drawGame() { //funciton for drawing the graphics
     ctx.drawImage(myPlayground, 0, 0); //draws the background
-    ctx.drawImage(food, BOX*foodCoords.x, BOX*foodCoords.y); //draws the background
+    ctx.drawImage(food, BOX * foodCoords.x, BOX * foodCoords.y); //draws the background
 
 
 }
