75ccacee9501324356f1fb1d90414ba57a6b46c9
PatriksM <pmorevs@gmail.com>
2026-02-06 15:00:01 +0200

    snake movement

--- Patric/SnakeGame/js/main.js
+++ Patric/SnakeGame/js/main.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('game'); //links the canvas tag to the ob
 const ctx = canvas.getContext('2d'); //enables use of getContext tools in html
 
 const BOX = 32;
-// total size of playground is 608 x 608, there are 19 squares in each direction, that means that each small square is of size 32x32
+// total size of playground is 608 x 608, there are 19 squares in each direction, that means that each small square is of size 32x32 so we make a vriable BOX = 32 to say the size of each little square
 
 let foodCoords = {
     x: Math.floor(1 + 17 * Math.random()),
@@ -27,13 +27,18 @@ function drawGame() { //funciton for drawing the graphics
     ctx.drawImage(myPlayground, 0, 0); //draws the background
     ctx.drawImage(food, BOX * foodCoords.x, BOX * foodCoords.y); //draws the background
 
-    ctx.fillStyle = "green";
-    ctx.fillRect(snakeCoords[0].x, snakeCoords[0].y, BOX, BOX);
+    for (let i = 0; i < snakeCoords.length; i++) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(snakeCoords[i].x, snakeCoords[i].y, BOX, BOX);
+    }
+
 
     snakeX = snakeCoords[0].x;
     snakeY = snakeCoords[0].y;
 
     snakeX += BOX;
+
+    snakeCoords.pop();
 
     let newHead = {
         x: snakeX,
@@ -41,6 +46,7 @@ function drawGame() { //funciton for drawing the graphics
     }
 
     snakeCoords.unshift(newHead);
+    console.log(snakeCoords);
 }
 
 myGame = setInterval(drawGame, 100); //refresh the drawn function each 100 ms. 
